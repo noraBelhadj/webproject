cf935ec10c8a407bb7d789d51ebfd62ae7937a40
Nora <nr.belhadj@gmail.com>
2019-05-07 17:21:27 +0200

    form

--- src/app/components/subscribe/subscribe.component.ts
+++ src/app/components/subscribe/subscribe.component.ts
@@ -22,20 +22,40 @@ export class SubscribeComponent implements OnInit {
   }
 
   initForm() {
-  	this.email = new FormControl('',[Validators.required,Validators.pattern("[^ @]*@[^ @]*")]);
-  	this.nom = new FormControl('', [Validators.required, Validators.minLength(4)]);
-  	this.prenom = new FormControl('', [Validators.required]);
-  	this.pays = new FormControl('', [Validators.required]);
-  	this.dateDeNaissance = new FormControl('', [Validators.required]);
+  	 this.email = new FormControl('',[Validators.required,Validators.pattern("[^ @]*@[^ @]*")]);
+  	//this.nom = new FormControl('', [Validators.required, Validators.minLength(4)]);
+  	//this.prenom = new FormControl('', [Validators.required]);
+  	//this.pays = new FormControl('', [Validators.required]);
+  	//this.dateDeNaissance = new FormControl('', [Validators.required]);
+    
+  //  this.infosForm = new FormGroup({
+  //      dateDeNaissance: this.dateDeNaissance,
+  //  	pays: this.pays}
+  //  )
+
     this.subscribeForm = new FormGroup({
-    	nom: this.nom,
-    	email: this.email,
-    	password: this.password}
-    	);
-    this.infosForm = new FormGroup({
-        dateDeNaissance: this.dateDeNaissance,
-    	pays: this.pays}
-    )
+    	//nom: this.nom,
+    	email: new FormControl('',[Validators.required,Validators.pattern("[^ @]*@[^ @]*")]),
+    	password: new FormControl ('', [Validators.required, Validators.minLength(5)]),
+    	infosPersonnel: new FormGroup({
+         nom: new FormControl('',[Validators.required]),
+         prenom: new FormControl('', [Validators.required]),
+         dateDeNaissance: new FormControl('',[Validators.required])
+    	}),
+    	Adresse: new FormGroup({
+    	  pays: new FormControl(),
+    	  rue: new FormControl(),
+    	  codePostale: new FormControl()
+    	})
+    	/*infosForm: this.infosForm*/
+    });
+  }
+
+   onSubmit() {
+    const values = this.subscribeForm.value;
+    const keys = Object.keys(values);
+    if (this.subscribeForm.valid) {
+      console.log(values);}
   }
 
 }

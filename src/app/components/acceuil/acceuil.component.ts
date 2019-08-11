6ea65efa00ecb314fa1f8036cd482b53feff1108
belhadj <nr.belhadj@gmail.com>
2019-08-11 21:14:56 +0200

    common modules

--- src/app/components/acceuil/acceuil.component.ts
+++ src/app/components/acceuil/acceuil.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subscriber } from 'rxjs/Subscriber';
+import { timer } from 'rxjs/observable/timer';
+
 
 @Component({
   selector: 'app-acceuil',
@@ -7,9 +11,119 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AcceuilComponent implements OnInit {
 
+
+
+
   constructor() { }
 
   ngOnInit() {
+  	const ARR = [1,2,3];
+
+  	ARR.length =2;
+  	console.log(ARR);
+    //alert(ARR);
+  	const personne = { name:"george", age:23};
+  	personne.name="name";
+  //	console.log(personne.name);
+//alert(personne.name);
+  	let pers = Object.freeze({name: "george", age:23});
+    //pers.name = "oe";
+  ///	console.log(pers.name);
+
+  	let x = 3+".05";
+  	//alert(x);
+
+  	var a,b,c;
+  	[a,b=2,c] = [1,2,{id:0}];
+
+  //	alert(c.id);
+
+  //  var o = new obj() //error
+
+
+  	var task1 = new Promise(function(resolve, reject) {
+    setTimeout(resolve, 1000, 'one');
+    });
+
+    var task2 = new Promise(function(resolve, reject) {
+    setTimeout(resolve, 2000, 'two');
+    });
+
+  Promise.race([task1, task2]).then(function(value) {
+ // console.log(value); // "one"
+  // Both resolve, but task1 resolves faster
+   });
+  //Promise.rej
+
+  const p = new Promise((resolve, reject) => { console.log("starting...");
+
+  //alert("starting...")
+
+  });
   }
 
 }
+
+
+ class Circle {
+ 	
+ 	
+
+ 	static area(r){
+
+
+		return (r*r*Math.PI);
+	} 
+
+ }
+	
+	let c = new Circle();
+ 
+
+	Circle.area(5);
+	//c.area(5);
+  //c.constructor.area(5);
+
+  
+  
+
+const observable = new Observable(subscriber=>{
+   subscriber.next(1);
+   subscriber.next('ghghg');
+   subscriber.next(3);
+
+   setTimeout(() => {
+    subscriber.next(4);
+    subscriber.complete();
+  }, 1000);
+ 
+
+});
+console.log('juste avant subscribe');
+
+observable.subscribe({
+next(x) { console.log('got value '+ x)},
+error(err){ console.log('error')},
+complete(){ console.log('done')}
+
+});
+
+console.log('juste apre subscribe');
+const test ="";
+
+const source = timer(1000);
+const subscribe = source.subscribe(val=> console.log(val));
+
+let isResolved = true;
+
+let myPromise = new Promise((resolve, reject) => {
+    if (isResolved) {
+        resolve((x)=> console.log('sddsdsdsdsdsdsdsd'));
+       
+    }
+    else {
+        let error = new Error("J'ai pas envie que ça marche");
+        ("Super ça marche");
+        reject(error);
+    }
+});
--- src/app/components/article/article.module.ts
+++ src/app/components/article/article.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 import { ArticleComponent }    from './article.component';
 import { ArticleRoutingModule} from './article-routing.module';
+import {SharedModule} from '../../shard/index';
 
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule,
     ReactiveFormsModule,
     FormsModule,
     ArticleRoutingModule
